812f5929b964b0bb043b494dc05d70026cca5691
NMburuga <157301672+NMburuga@users.noreply.github.com>
2024-08-08 10:10:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def add_background_image(image_file):
     st.markdown(css, unsafe_allow_html=True)
 
 # Add your background image from the local file in the repo
-add_background_image('rice_farmer.jpeg')
+add_background_image('rice_grain.jpeg')
 
 # Load the pickled model
 with open('model.pkl', 'rb') as f:
